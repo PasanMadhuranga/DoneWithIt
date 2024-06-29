e0c96e50139f3ae271525c6990ebff5943f31c75
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-29 21:30:36 +0530

    flexBox

--- App.js
+++ App.js
@@ -1,34 +1,31 @@
 // import { StatusBar } from "expo-status-bar";
 import {
-  StyleSheet,
-  Text,
   View,
-  SafeAreaView,
-  Image,
-  TouchableOpacity,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-  Dimensions,
 } from "react-native";
 import { useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
-  const orientation = useDeviceOrientation()
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={{ flex: 1}}>
       <View
-        style={{ backgroundColor: "red", width: "50%", height: orientation === "landscape" ? "100%" : "30%" }}
-      ></View>
-    </SafeAreaView>
+        style={{
+          backgroundColor: "dodgerblue",
+         flex: 3,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "red",
+         flex: 2,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "orange",
+         flex: 2,
+        }}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "yellow",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
