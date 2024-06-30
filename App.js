1c7716658ed23f8fc9c36f904db7d5aa87eeda65
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-30 08:39:06 +0530

    border

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { View } from "react-native";
 
 export default function App() {
   return (
     // <WelcomeScreen />
-    <ViewImageScreen />
+    // <ViewImageScreen />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderColor: "gold",
+          borderWidth: 10,
+          borderRadius: 30,
+          borderTopLeftRadius: 50,
+          borderBottomWidth: 20,
+          }}
+      ></View>
+    </View>
   );
 }
