cb7cd71a9e69e05df8480b74f5ecfbbece9ffc5c
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-30 09:40:15 +0530

    icons

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import AppText from "./app/components/AppText";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import {MaterialCommunityIcons} from "@expo/vector-icons"
 
 export default function App() {
   return (
     // <WelcomeScreen />
     // <ViewImageScreen />
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <AppText>
-        Hello World lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-      </AppText>
+      <MaterialCommunityIcons name="email" size={100} color="pink" />
     </View>
   );
 }
