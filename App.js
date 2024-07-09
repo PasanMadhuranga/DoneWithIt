eafdda76140d590504382afae3371bff329b5563
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-09 17:51:19 +0530

    finish custom picker

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-
+import { useState } from "react";
 
 import Screen from "./app/components/Screen";
 import AppPicker from "./app/components/AppPicker";
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
+
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
     <GestureHandlerRootView>
       <Screen>
-        <AppPicker icon={"apps"} placeholder={"Category"} />
+        <AppPicker 
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        icon={"apps"} placeholder={"Category"} items={categories} />
       </Screen>
     </GestureHandlerRootView>
   );
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -7,14 +7,16 @@ import {
   TouchableWithoutFeedback,
   Modal,
   Button,
+  FlatList,
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import defaultStyles from "../config/styles";
 import AppText from "./AppText";
 import Screen from "./Screen";
+import PickerItem from "./PickerItem";
 
-function AppPicker({ icon, placeholder, ...OtherProps }) {
+function AppPicker({ icon, placeholder, items, selectedItem, onSelectItem }) {
   const [modalVisible, setModalVisible] = useState(false);
   return (
     <>
@@ -28,7 +30,9 @@ function AppPicker({ icon, placeholder, ...OtherProps }) {
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>{placeholder}</AppText>
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}
+          </AppText>
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -37,7 +41,24 @@ function AppPicker({ icon, placeholder, ...OtherProps }) {
         </View>
       </TouchableWithoutFeedback>
       <Modal visible={modalVisible} animationType="slide">
-          <Button title="Close" onPress={() => setModalVisible(false)} />
+        <Button
+          title="Close"
+          onPress={() => setModalVisible(false)}
+          color={defaultStyles.colors.medium}
+        />
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.value.toString()}
+          renderItem={({ item }) => (
+            <PickerItem
+              label={item.label}
+              onPress={() => {
+                setModalVisible(false);
+                onSelectItem(item);
+              }}
+            />
+          )}
+        />
       </Modal>
     </>
   );
