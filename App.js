020cbc103e08410b6681c58ec31604a6803ab030
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-09 09:39:03 +0530

    create modal

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import { View, TextInput } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { useState } from "react";
 
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+
 import Screen from "./app/components/Screen";
-import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
   return (
     <GestureHandlerRootView>
       <Screen>
-        <AppTextInput placeholder="Username" icon="email" keyboardType="numeric" />
+        <AppPicker icon={"apps"} placeholder={"Category"} />
       </Screen>
     </GestureHandlerRootView>
   );
