6b5630eceb4baef602116ea1222322d39c8cb093
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-12 22:08:08 +0530

    accessing gallery

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as ImagePicker from "expo-image-picker";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Image } from "react-native";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert("You need to enable permission to access the library.");
@@ -14,9 +16,22 @@ export default function App() {
     requestPermission();
   }, []);
 
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  }
+
   return (
     <GestureHandlerRootView>
       <Screen>
+        <Button title="Select Image" onPress={selectImage} />
+        <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
         </Screen>
     </GestureHandlerRootView>
   );
