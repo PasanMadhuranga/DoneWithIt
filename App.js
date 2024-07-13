2216692b2ce5bb10e00614a81d247b1d7d71001b
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-13 16:33:52 +0530

    navigationTheme

--- App.js
+++ App.js
@@ -1,91 +1,15 @@
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Button, Text } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
-import Screen from "./app/components/Screen";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
 
-const Link = () => {
-  const navigation = useNavigation();
-  return (
-    <Button
-      title="Click Meeeeee!"
-      onPress={() => navigation.navigate("TweetDetails", { id: "pasan" })}
-    />
-  );
-};
-
-const Tweets = () => {
-  return (
-    <Screen>
-      <Text>Tweets</Text>
-      <Link />
-    </Screen>
-  );
-};
-
-const TweetDetails = ({ route }) => {
-  return (
-    <Screen>
-      <Text>Tweet Details {route.params.id}</Text>
-    </Screen>
-  );
-};
-
-const Account = () => {
-  return (
-    <Screen>
-      <Text>Account</Text>
-    </Screen>
-  );
-};
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarActiveBackgroundColor: "tomato",
-        tabBarInactiveBackgroundColor: "#eee",
-        tabBarActiveTintColor: "white",
-        tabBarInactiveTintColor: "black",
-        headerShown: false,
-      }}
-    >
-      <Tab.Screen name="Feed" component={FeedNavigator} />
-      <Tab.Screen name="Account" component={Account} />
-    </Tab.Navigator>
-  );
-};
-
-const FeedNavigator = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: "gold" },
-        headerTintColor: "white",
-        // headerShown: false,
-      }}
-    >
-      <Stack.Screen name="Tweets" component={Tweets} />
-      <Stack.Screen
-        name="TweetDetails"
-        component={TweetDetails}
-        options={({ route }) => ({ title: route.params.id })}
-      />
-    </Stack.Navigator>
-  );
-};
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <NavigationContainer>
+      <NavigationContainer theme={navigationTheme}>
         <AuthNavigator />
       </NavigationContainer>
     </GestureHandlerRootView>
