5ea67b24e14b0968da0563b7d6858eedb6a4030a
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-13 17:33:03 +0530

    FeedNavigator

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -20,20 +20,21 @@ const listings = [
   },
 ];
 
-function ListingsScreen(props) {
+function ListingsScreen({ navigation }) {
   return (
     <Screen style={styles.screen}>
-        <FlatList
-          data={listings}
-          keyExtractor={(listing) => listing.id.toString()}
-          renderItem={({ item }) => (
-            <Card
-              title={item.title}
-              subTitle={"$" + item.price.toLocaleString()}
-              image={item.image}
-            />
-          )}
-        />
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={"$" + item.price.toLocaleString()}
+            image={item.image}
+            onPress={() => navigation.navigate("ListingDetails", item)}
+          />
+        )}
+      />
     </Screen>
   );
 }
@@ -44,6 +45,5 @@ const styles = StyleSheet.create({
     padding: 20,
   },
 });
-
 
 export default ListingsScreen;
