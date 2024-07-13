6a460fcc49a406a232ed6ca52767d36497e0dfd5
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-13 15:25:44 +0530

    stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
+import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+
+const Tweets = () => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+    </Screen>
+  );
+};
+
+const TweetDetails = () => {
+  return (
+    <Screen>
+      <Text>Tweet Details</Text>
+    </Screen>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-
   return (
     <GestureHandlerRootView>
-      <ListingEditScreen /> 
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
